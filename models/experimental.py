d86adcf4b17ea39aa6ac978c8b85637edf6e7b07
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-11 19:59:00 -0800

    cleanup

--- models/experimental.py
+++ models/experimental.py
@@ -71,6 +71,20 @@ class C3b(nn.Module):
         return self.act(self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1)))
 
 
+class C3c(nn.Module):
+    # Bottleneck with 2 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super(C3c, self).__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1, 1, act=FReLU(c2))
+        self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
+
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
+
+
 class Sum(nn.Module):
     # Weighted sum of 2 or more layers https://arxiv.org/abs/1911.09070
     def __init__(self, n, weight=False):  # n: number of inputs
